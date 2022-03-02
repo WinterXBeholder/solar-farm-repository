9243fa2dd33f6d14ae7c3c477cbfdcb92d03abe6
Brian <brian.hacker@ire.minnate.edu>
2022-03-02 15:18:33 -0600

    implemented Spring XML dependency configuration

--- src/main/java/learn/solar/App.java
+++ src/main/java/learn/solar/App.java
@@ -4,15 +4,23 @@ import learn.solar.data.PanelFileRepository;
 import learn.solar.domain.PanelService;
 import learn.solar.ui.Controller;
 import learn.solar.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
+        Controller controller = context.getBean(Controller.class);
+        controller.run();
+        /*
         PanelFileRepository repository = new PanelFileRepository("./data/panels.csv");
         PanelService service = new PanelService(repository);
         View view = new View();
 
         Controller controller = new Controller(service, view);
         controller.run();
+         */
     }
 }
